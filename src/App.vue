742179cc94df2cc4eb3755cf8b163b55cc211897
shogosakai <shogosakai73@gmail.com>
2020-07-23 22:55:23 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="main">
+    <button @click="show = !show">切り替えボタン</button>
+    <transition name="fade">
+    <p v-if="show">hello</p>
+    </transition>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+      show: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+.fade-enter{}
+.fade-enter-active{}
+.fade-enter-to{}
+.fade-leave{}
+.fade-leave-active{}
+.fade-leave-to{}
+.main{
+  width: 70%;
+  margin: auto;
+  padding-top: 5rem;
+  text-align: center;
+}
+</style>
