fe6b4da406afcfb37db5b77ea41f212640a86aec
shogosakai <shogosakai73@gmail.com>
2020-07-23 23:07:41 +0900

    add fade in out transitions.

--- src/App.vue
+++ src/App.vue
@@ -18,12 +18,30 @@ export default {
 </script>
 
 <style scoped>
-.fade-enter{}
-.fade-enter-active{}
-.fade-enter-to{}
-.fade-leave{}
-.fade-leave-active{}
-.fade-leave-to{}
+.fade-enter{
+  /* 現れるときの最初の状態 */
+  opacity: 0;
+}
+.fade-enter-active{
+  /* 現れるときのトランジションの状態 */
+  transition: opacity 0.5s;
+}
+.fade-enter-to{
+  /* 現れるときの最後の状態 */
+  opacity: 1;
+}
+.fade-leave{
+  /* 消えるときの最初の状態 */
+  opacity: 1;
+}
+.fade-leave-active{
+  /* 消えるときのトランジションの状態 */
+  transition: opacity 0.5s;
+}
+.fade-leave-to{
+  /* 消えるときの最後の状態 */
+  opacity: 0;
+}
 .main{
   width: 70%;
   margin: auto;
